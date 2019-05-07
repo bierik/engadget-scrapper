e1b6b28ce691d1b47352c8ff7c0c887b0c09a349
Kevin Bieri <bierike@gmail.com>
2019-05-07 10:54:43 +0200

    Get article urls lazily
    This will speed up the scrapping process a lot. Also get the total num pages from the news page itself.

--- engadet/spiders/EngadgetNews.py
+++ engadet/spiders/EngadgetNews.py
@@ -7,6 +7,11 @@ import requests
 class EngadgetnewsSpider(scrapy.Spider):
     name = 'EngadgetNews'
 
+    def get_num_pages(self):
+        url = "https://www.engadget.com/a/search?filters[page][type][]=articles&sort_field[page]=published_at"
+        response = requests.get(url)
+        return json.loads(response.text)["info"]["page"]["num_pages"]
+
     def clean_output_file(self):
         open('articles.json', 'w').close()
 
@@ -21,17 +26,13 @@ class EngadgetnewsSpider(scrapy.Spider):
         return " ".join(response.css('.article-text > p *::text').getall())
 
     def get_article_urls(self, pages):
-        articles = []
-
         for page in range(1, pages + 1):
             for article in self.get_articles_for_page(page):
-                articles.append(article['url'])
-
-        return articles
+                yield article['url']
 
     def start_requests(self):
         self.clean_output_file()
-        total_num_pages = 47322
+        total_num_pages = self.get_num_pages()
 
         for url in self.get_article_urls(total_num_pages):
             yield scrapy.Request(url=url, callback=self.parse)
