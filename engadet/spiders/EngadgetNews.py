a18c13801f32046e0196b555fb55150c30a4ae5e
Kevin Bieri <bierike@gmail.com>
2019-05-07 14:10:49 +0200

    Use archive page to fetch articles
    The search endpoint was somehow stuck on a specific page

--- engadet/spiders/EngadgetNews.py
+++ engadet/spiders/EngadgetNews.py
@@ -2,40 +2,56 @@
 import scrapy
 import json
 import requests
+from bs4 import BeautifulSoup
 
 
 class EngadgetnewsSpider(scrapy.Spider):
     name = 'EngadgetNews'
 
-    def get_num_pages(self):
-        url = "https://www.engadget.com/a/search?filters[page][type][]=articles&sort_field[page]=published_at"
+    def get_total_aricles(self):
+        url = "https://www.spot.im/api/init/spot/sp_f76RE06N"
         response = requests.get(url)
-        return json.loads(response.text)["info"]["page"]["num_pages"]
+        return json.loads(response.text)["newsfeed"]["total_items"]
+
+    def get_articles_for_page(self, page):
+        base_url = "https://www.engadget.com/all/page/"
+        return f"{base_url}{page}"
+
+    def get_absolute_article_url(self, url):
+        base_url = "https://www.engadget.com"
+        return f"{base_url}{url}"
 
     def clean_output_file(self):
         open('articles.json', 'w').close()
 
-    def create_page_url(self, page):
-        return f"https://www.engadget.com/a/search?filters[page][type][]=articles&sort_field[page]=published_at&page={page}"
-
-    def get_articles_for_page(self, page):
-        response = requests.get(self.create_page_url(page))
-        return json.loads(response.text)["records"]["page"]
+    def get_article_urls_for_page(self, page):
+        response = requests.get(self.get_articles_for_page(page))
+        html = response.text
+        parsed_html = BeautifulSoup(html, 'html.parser')
+        links = parsed_html.body.select('.o-hit__link', href=True)
+        urls = []
+        for link in links:
+            urls.append(link['href'])
+        return {
+            'total_articles': len(links),
+            'urls': urls,
+        }
 
     def extract_text(self, response):
         return " ".join(response.css('.article-text > p *::text').getall())
 
-    def get_article_urls(self, pages):
-        for page in range(1, pages + 1):
-            for article in self.get_articles_for_page(page):
-                yield article['url']
-
     def start_requests(self):
         self.clean_output_file()
-        total_num_pages = self.get_num_pages()
-
-        for url in self.get_article_urls(total_num_pages):
-            yield scrapy.Request(url=url, callback=self.parse)
+        total_articles = self.get_total_aricles()
+        processed_articles = 0
+        current_page = 0
+        while processed_articles < total_articles:
+            articles = self.get_article_urls_for_page(current_page)
+            current_page += 1
+            processed_articles += articles['total_articles']
+            for url in articles['urls']:
+                yield scrapy.Request(url=self.get_absolute_article_url(url),
+                                     callback=self.parse)
 
     def parse(self, response):
         yield {
